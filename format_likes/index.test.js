a308ae81a9468caa488f7952c88dec270645659e
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2020-12-15 15:08:28 -0300

    adaptação do test para ficar próximo ao executado na plataforma codewars

--- format_likes/index.test.js
+++ format_likes/index.test.js
@@ -1,16 +1,16 @@
-const { assert } = require("chai");
+const Test = require("chai");
 const { likes } = require("./index.js");
 
 describe("example tests", function () {
   it("should return correct text", function () {
-    assert.equal(likes([]), "no one likes this");
-    assert.equal(likes(["Peter"]), "Peter likes this");
-    assert.equal(likes(["Jacob", "Alex"]), "Jacob and Alex like this");
-    assert.equal(
+    Test.assert.equal(likes([]), "no one likes this");
+    Test.assert.equal(likes(["Peter"]), "Peter likes this");
+    Test.assert.equal(likes(["Jacob", "Alex"]), "Jacob and Alex like this");
+    Test.assert.equal(
       likes(["Max", "John", "Mark"]),
       "Max, John and Mark like this"
     );
-    assert.equal(
+    Test.assert.equal(
       likes(["Alex", "Jacob", "Mark", "Max"]),
       "Alex, Jacob and 2 others like this"
     );
